305708ff00d89e15482bc85cf453efedf1158858
esraasalah97 <46899002+esraasalah97@users.noreply.github.com>
2019-04-03 19:10:25 +0200

    add esraa functions

--- Team1/src/vehicle/app/State.java
+++ Team1/src/vehicle/app/State.java
@@ -4,6 +4,10 @@ import vehicle.hal.Temp;
 
 public class State {
 	public static int validateCall = 0 ;
+	public boolean bValidateLowPerformance()
+	{}
+	public void vidGoToNextState()
+	{}
 	public boolean bValidateHighPerformance() {
 		
 		 int temp_reading = Temp.iReadT1();
