384f6a8e5c10c98a1b351371edf9470b99618420
MarwaSheshtawy <46898852+MarwaSheshtawy@users.noreply.github.com>
2019-04-04 13:12:11 +0200

    State Class Updated with new Sequence and Validation calls assertions

--- Team1/src/vehicle/app/State.java
+++ Team1/src/vehicle/app/State.java
@@ -3,13 +3,28 @@ package vehicle.app;
 import vehicle.hal.Temp;
 
 public class State {
-	public static int validateCall = 0 ;
+	public static int validateCall1 = 0 ;
+	public static int validateCall2= 0 ;
+	public static int validateCall3 = 0 ;
+	public static int validateseq;
+	
+	
+	public boolean bValidateFailure() {
+		validateCall3++;
+		validateseq=1;
+		return false;
+	}
 	public boolean bValidateLowPerformance()
-	{return false;}
-	public void vidGoToNextState()
-	{}
+	{
+		validateCall1++;
+		validateseq=2;
+		return false;
+	}
+	
 	
 	public boolean bValidateHighPerformance() {
+		validateCall2++;
+		validateseq=3;
 		
 		 int temp_reading = Temp.iReadT1();
 		 if(temp_reading<=50) {
@@ -19,9 +34,11 @@ public class State {
 			 return false;
 		 }
 	}
-	public boolean bValidateFailure() {
-		// TODO Auto-generated method stub
-		return false;
+	public void vidGoToNextState()
+	{
+		Global.iState=4;
+	
 	}
+	
 
 }
